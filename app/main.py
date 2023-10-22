fb88cbc8bf4a9eff6c15e58f9604de449fa899c0
Daniel L. Alves <daniel.leonssio@hotmail.com>
2023-10-22 19:15:03 -0300

    Adicionando etapa de transform e load

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import concatenate
+from pipeline.load import load_excel
 
-dataframe_list = extract_from_excel('data/input')
-
-print(dataframe_list)
+if __name__ == '__main__':
+    df_list = extract_from_excel('data/input')
+    df = concatenate(df_list)
+    load_excel(df, "data/output", "output")
---
+++ app/pipeline/load.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+
+def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> str:
+
+    """
+    Receber um dataframe e salva-lo com excel
+
+    args:
+
+    data_frame: pd.DataFrame: Dataframe a ser salvo
+    output_path: (str): Caminho onde o arquivo será salvo
+    file_name: (str): Nome do arquivo a ser salvo
+
+    return: (str): "Arquivo salvo com sucesso"
+
+    """
+
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index=False)
+
+    return "Arquivo salvo com sucesso"
