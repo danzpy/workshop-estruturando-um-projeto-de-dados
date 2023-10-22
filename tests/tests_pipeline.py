2814078c8195ff99aa262bc14787a287b51cd428
Daniel L. Alves <daniel.leonssio@hotmail.com>
2023-10-22 18:30:16 -0300

    Iniciando etapa de testes

---
+++ tests/tests_pipeline.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from app.pipeline.transform import concatenate
+
+df1 = pd.DataFrame({'col1':[1, 2], 'col2': [3, 4]})
+df2 = pd.DataFrame({'col1':[5, 6], 'col2': [7, 8]})
+
+
+def concatenar_lista_df():
+    arrange = pd.concat([df1, df2], ignore_index=True)
+
+    act = concatenate([df1, df2])
+
+    assert arrange == act
