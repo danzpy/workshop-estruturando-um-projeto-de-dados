94c72592decc090d081f00e14b2ba4db7e3caf76
Daniel L. Alves <daniel.leonssio@hotmail.com>
2023-10-22 18:52:25 -0300

    Adicionando etapa de transform

--- tests/tests_pipeline.py
+++ tests/tests_pipeline.py
@@ -4,10 +4,16 @@ from app.pipeline.transform import concatenate
 df1 = pd.DataFrame({'col1':[1, 2], 'col2': [3, 4]})
 df2 = pd.DataFrame({'col1':[5, 6], 'col2': [7, 8]})
 
+"""
 
-def concatenar_lista_df():
+Função para utilizar o arrange e act para testar a função concatenate
+
+"""
+
+
+def test_concatenar_lista_df():
     arrange = pd.concat([df1, df2], ignore_index=True)
 
     act = concatenate([df1, df2])
 
-    assert arrange == act
+    assert arrange.equals(act)
