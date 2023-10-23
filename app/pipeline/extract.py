84aba9005a9a666d6ca1f51504d1e21d815c7a7d
Daniel L. Alves <daniel.leonssio@hotmail.com>
2023-10-23 17:34:16 -0300

    Ajustando docstring das funções

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -3,16 +3,18 @@ import glob # para listar arquivos
 import pandas as pd
 from typing import List
 
-"""
-    Função para ler os arquivos de uma pasta "data/input"
-    e retornar uma lista de dataframes
-    args: input_path (str): caminho da pasta
-    return: list (list): lista de dataframes existentes no input_path
-"""
+
 
 path = 'data/input'
 
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
+
+    """
+    Função para ler os arquivos de uma pasta "data/input"
+    e retornar uma lista de dataframes
+    args: input_path (str): caminho da pasta
+    return: list (list): lista de dataframes existentes no input_path
+    """
 
     all_files = glob.glob(f'{path}/*.xlsx')
 
--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from typing import List
 
-"""
-função para transformar uma lista de dataframes em um único dataframe
-
-args:
-
-dataframe_list: list
-
-"""
-
 def concatenate(dataframe_list: List[pd.DataFrame]) -> pd.DataFrame:
+    
+    """
+    função para transformar uma lista de dataframes em um único dataframe
+
+    args:
+
+    dataframe_list: list
+
+    """
+
     return pd.concat(dataframe_list, ignore_index=True)
